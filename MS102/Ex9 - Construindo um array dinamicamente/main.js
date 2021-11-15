47500763929e68a8cf3d4c0e784ecc771251802d
Gabriel Beserra <gabriel.fernandesb@gmail.com>
2021-11-15 13:22:54 -0300

    Exercício 9: criando um array dinâmico

---
+++ MS102/Ex9 - Construindo um array dinamicamente/main.js
@@ -0,0 +1,49 @@
+window.onload = function () {
+  var answer = ''
+  var arr = []
+  var colors = [
+    'pink',
+    'blue',
+    'orange',
+    'white',
+    'green',
+    'red',
+    'violet',
+    'grey',
+    'brown',
+    'yellow'
+  ]
+
+  while (answer != 'parar') {
+    answer = prompt(`Digite uma palavra (escreva "parar" para sair).`)
+    if (answer != 'parar') {
+      arr.push(answer)
+    }
+  }
+
+  // Descend sorting array
+  arr.sort(function (b, a) {
+    if (a > b) {
+      return 1
+    }
+    if (a < b) {
+      return -1
+    }
+    return 0
+  })
+
+  // Creating sections
+  var container = document.getElementById('container')
+  for (let i = 0; i < arr.length; i++) {
+    var section = document.createElement('section')
+    section.id = 'section' + i
+    section.style.backgroundColor =
+      colors[Math.floor(Math.random() * colors.length)]
+
+    var h1 = document.createElement('h1')
+    h1.innerHTML = arr[i]
+
+    container.appendChild(section)
+    section.appendChild(h1)
+  }
+}
