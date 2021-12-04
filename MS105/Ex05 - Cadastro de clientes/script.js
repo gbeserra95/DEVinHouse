3fa15bd9887c94e2ec07a241ff50aa4085089bc7
Gabriel Beserra <gabriel.fernandesb@gmail.com>
2021-12-04 09:43:26 -0300

    Exercicio classes 5 e 6

--- MS105/Ex05 - Cadastro de clientes/script.js
+++ MS105/Ex05 - Cadastro de clientes/script.js
@@ -1,110 +1,39 @@
-// My local storage
-var myLocalStorage = 'myCustomer'
-
-// Customer
-const customerName = document.getElementById('customer-name')
-const cpf = document.getElementById('cpf')
-const phoneNumber = document.getElementById('phone-number')
-
-// Address
-const addressType = document.getElementById('address-type')
-const street = document.getElementById('street')
-const number = document.getElementById('number')
-const suburb = document.getElementById('suburb')
-const city = document.getElementById('city')
-const state = document.getElementById('state')
-const zipCode = document.getElementById('zip')
-
-// Enable submit button when inputs are filled
-let myInputs = document.querySelectorAll("input[type='text']")
-
-// My submit button
-let myBtn = document.getElementById('btn-submit')
-
-for (let i = 0; i < myInputs.length; i++) {
-  myInputs[i].addEventListener('input', function () {
-    let isBlank = 0
-    for (let j = 0; j < myInputs.length; j++) {
-      if (myInputs[j].value != '') {
-        isBlank++
-      }
-    }
-    if (isBlank == myInputs.length) {
-      myBtn.disabled = false
-    } else {
-      myBtn.disabled = true
-    }
-  })
-}
-
-class Customer {
-  constructor(
-    customerName,
-    cpf,
-    phoneNumber,
-    type,
-    street,
-    number,
-    suburb,
-    city,
-    state,
-    zip
-  ) {
-    this.customerName = customerName
-    this.cpf = cpf
-    this.phoneNumber = phoneNumber
+class Address {
+  constructor(type, street, number, city, state, country, zip) {
     this.type = type
     this.street = street
     this.number = number
-    this.suburb = suburb
     this.city = city
     this.state = state
+    this.country = country
     this.zip = zip
   }
+}
 
-  get customerData() {
-    return [
-      this.customerName,
-      this.cpf,
-      this.phoneNumber,
-      this.type,
-      this.street,
-      this.number,
-      this.suburb,
-      this.city,
-      this.state,
-      this.zip
-    ]
+class Customer {
+  constructor(customerName, cpf, address, phone) {
+    this.customerName = customerName
+    this.cpf = cpf
+    this.address = address
+    this.phone = phone
   }
 }
 
-// get the current local storage JSON
-function getLocalStorage() {
-  if (localStorage.getItem(myLocalStorage)) {
-    return JSON.parse(localStorage.getItem(myLocalStorage))
-  } else {
-    localStorage.setItem(myLocalStorage, '[]')
-    return JSON.parse(localStorage.getItem(myLocalStorage))
-  }
-}
+let newAddress = new Address(
+  'Comercial ',
+  'Rua Antônio da Veiga',
+  '479',
+  'Blumenau',
+  'SC',
+  'Brasil',
+  '89012-500'
+)
 
-// Save data on local Storage
-function saveData() {
-  let myObj = getLocalStorage()
+let newCustomer = new Customer(
+  'Dominos Pizza',
+  '11111111111',
+  newAddress,
+  '4732377778'
+)
 
-  let customer = new Customer(
-    customerName.value,
-    cpf.value,
-    phoneNumber.value,
-    addressType.value,
-    street.value,
-    number.value,
-    suburb.value,
-    city.value,
-    state.value,
-    zip.value
-  )
-
-  myObj.push(customer)
-  localStorage.setItem(myLocalStorage, JSON.stringify(myObj))
-}
+console.log(newCustomer)
--- MS105/Ex06 - Método para criação de conta/script.js
+++ MS105/Ex06 - Método para criação de conta/script.js
@@ -1,112 +1,49 @@
-// My local storage
-var myTableHeader = 'myTableHeader'
-var myLocalStorage = 'myCustomer'
-
-// Customer
-const customerName = document.getElementById('customer-name')
-const cpf = document.getElementById('cpf')
-const phoneNumber = document.getElementById('phone-number')
-
-// Address
-const addressType = document.getElementById('address-type')
-const street = document.getElementById('street')
-const number = document.getElementById('number')
-const suburb = document.getElementById('suburb')
-const city = document.getElementById('city')
-const state = document.getElementById('state')
-const zipCode = document.getElementById('zip')
-
-// Enable submit button when inputs are filled
-let myInputs = document.querySelectorAll("input[type='text']")
-
-// My submit button
-let myBtn = document.getElementById('btn-submit')
-
-for (let i = 0; i < myInputs.length; i++) {
-  myInputs[i].addEventListener('input', function () {
-    let isBlank = 0
-    for (let j = 0; j < myInputs.length; j++) {
-      if (myInputs[j].value != '') {
-        isBlank++
-      }
-    }
-    if (isBlank == myInputs.length) {
-      myBtn.disabled = false
-    } else {
-      myBtn.disabled = true
-    }
-  })
-}
-
-class Customer {
-  constructor(
-    customerName,
-    cpf,
-    phoneNumber,
-    type,
-    street,
-    number,
-    suburb,
-    city,
-    state,
-    zip
-  ) {
-    this.customerName = customerName
-    this.cpf = cpf
-    this.phoneNumber = phoneNumber
+class Address {
+  constructor(type, street, number, city, state, country, zip) {
     this.type = type
     this.street = street
     this.number = number
-    this.suburb = suburb
     this.city = city
     this.state = state
+    this.country = country
     this.zip = zip
   }
+}
 
-  static dataHeader() {
-    return [
-      'Cliente',
-      'CPF',
-      'Telefone',
-      'Endereço',
-      'Logradouro',
-      'Número',
-      'Bairro',
-      'Cidade',
-      'Estado',
-      'CEP'
-    ]
+class Customer {
+  constructor(customerName, cpf, address, phone) {
+    this.customerName = customerName
+    this.cpf = cpf
+    this.address = address
+    this.phone = phone
   }
 }
 
-// get the current local storage JSON
-function getLocalStorage() {
-  if (localStorage.getItem(myLocalStorage)) {
-    return JSON.parse(localStorage.getItem(myLocalStorage))
-  } else {
-    localStorage.setItem(myTableHeader, JSON.stringify(Customer.dataHeader()))
-    localStorage.setItem(myLocalStorage, '[]')
-    return JSON.parse(localStorage.getItem(myLocalStorage))
+class Account {
+  constructor(account, balance, customer) {
+    this.account = account
+    this.balance = balance
+    this.customer = customer
   }
 }
 
-// Save data on local Storage
-function saveData() {
-  let myObj = getLocalStorage()
-  let customer = new Customer(
-    customerName.value,
-    cpf.value,
-    phoneNumber.value,
-    addressType.value,
-    street.value,
-    number.value,
-    suburb.value,
-    city.value,
-    state.value,
-    zipCode.value
-  )
+let newAddress = new Address(
+  'Comercial ',
+  'Rua Antônio da Veiga',
+  '479',
+  'Blumenau',
+  'SC',
+  'Brasil',
+  '89012-500'
+)
 
-  myObj.push(customer)
-  console.log(myObj)
-  localStorage.setItem(myLocalStorage, JSON.stringify(myObj))
-}
+let newCustomer = new Customer(
+  'Dominos Pizza',
+  '11111111111',
+  newAddress,
+  '4732377778'
+)
+
+let newAccount = new Account('256987-8', 'R$2000000,00', newCustomer)
+
+console.log(newAccount)
