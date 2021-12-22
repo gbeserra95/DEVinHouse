4f62dcec027094105761c593ba0d6879f88e2fcb
Gabriel Beserra <gabriel.fernandesb@gmail.com>
2021-12-22 11:57:35 -0300

    Incompleto...exercicio 08, primeiros arquivos

---
+++ MS106/Ex08 - CSS customizado/modules/questions.js
@@ -0,0 +1,152 @@
+export const questions = [
+    {
+        question: [
+            "https://kanto.legiaodosherois.com.br/w750-h450-k1/wp-content/uploads/2021/11/legiao_le7anhg1zXSR.png.jpeg",
+            "Which actor is also the screenwriter for The Office?"
+        ],
+        choices: {
+            "John Krasinski (Jim Halpert)": false,
+            "Jenna Fischer (Pam Beesly)": false,
+            "Angela Kinsey (Angela Martin)": false,
+            "None of them": true
+        },
+        answer: [
+            "https://kanto.legiaodosherois.com.br/w750-h750-k1/wp-content/uploads/2021/11/legiao_ZiqhcAITUg4l.png.jpeg",
+            "Many actors on The Office were screenwriters for the show, but none of the names cited fit this. Acting and writing the series were BJ Novak (Ryan), Mindy Kaling (Kelly) and Paul Lieberstein (Toby)."
+        ]
+    },
+    {
+        question: [
+            "https://kanto.legiaodosherois.com.br/w750-h450-k1/wp-content/uploads/2021/11/legiao_kUJW_zgNaMV5.png.jpeg",
+            "Creed Bratton's character is based on a real person."
+        ],
+        choices: {
+            "True": true,
+            "False": false,
+        },
+        answer: [
+            "https://kanto.legiaodosherois.com.br/w750-h750-k1/wp-content/uploads/2021/11/legiao_1crAbdi5wyDx.png.jpeg",
+            "Creed Bratton is based on Creed Bratton, the actor who plays him on the show. Seriously, the character is a fictional version of the interpreter, who in real life is a musician in a band called The Grass Roots - something that is even mentioned in deleted scenes from the series. He has 12 albums out, the most recent having come out in 2020."]
+    },
+    {
+        question: [
+            "https://kanto.legiaodosherois.com.br/w750-h450-k1/wp-content/uploads/2021/11/legiao_qdkWi3HgIe8t.png.jpeg",
+            "The Office opening was directed by John Krasinski"
+        ],
+        choices: {
+            "True": true,
+            "False": false,
+        },
+        answer: [
+            "https://kanto.legiaodosherois.com.br/w750-h750-k1/wp-content/uploads/2021/11/legiao_Wa_r9Jzj4YT7.png.jpeg",
+            "Long before Jim Halpert actor became known for directing the films A Quiet Place, John Krasinski directed the opening of The Office. Creating the iconic opening wasn't the intention, in fact: he went to Scranton, Pennsylvania, to research the paper, interviewing several employees of paper companies to get a better sense of the daily life. During the trip with some friends, Krasinski recorded several scenes around town, and this material ended up being used for the opening credits of the program."
+        ]
+    },
+    {
+        question: [
+            "https://kanto.legiaodosherois.com.br/w750-h450-k1/wp-content/uploads/2021/11/legiao_gD8WQ7voLdxF.png.jpeg",
+            "How many actors from The Office - recurring or guest appearances - ended up in Marvel's Cinematic Universe?"
+        ],
+        choices: {
+            "1": false,
+            "2": false,
+            "3": false,
+            "4": true
+        },
+        answer: [
+            "https://kanto.legiaodosherois.com.br/w750-h750-k1/wp-content/uploads/2021/11/legiao_yvdG54YwiqpA.png.jpeg",
+            "Ainda que tenha uma enorme lista de participações especiais, não são tantos atores de The Office que conquistaram vagas no MCU depois. Na verdade, foram apenas 4. Evan Peters, o Mercúrio/Ralph Bonner de WandaVision, apareceu como sobrinho de Michael Scott em um episódio; Idris Elba, o Heimdall dos filmes do Thor, interpretou o executivo Charles Miner por alguns episódios. Já James Spader, que viveu o chefe Robert California, é o responsável por dar voz ao robô Ultron em Vingadores: A Era de Ultron. Por fim, Randall Park - o Jimmy Woo de WandaVision - teve uma breve e memorável aparição como o Jim asiático em uma pegadinha com Dwight."
+        ]
+    },
+    {
+        question: [
+            "https://kanto.legiaodosherois.com.br/w750-h450-k1/wp-content/uploads/2021/11/legiao_Fixa723AbXRT.png.jpeg",
+            "Who of these people has NOT directed an episode of The Office?"
+        ],
+        choices: {
+            "Jon Favreau (Director of Iron Man, Happy Hogan's actor on the MCU)": false,
+            "Bryan Cranston (Walter White's actor in Breaking Bad)": false,
+            "Jenna Fischer (Pam's actress in The Office)": true,
+            "Rainn Wilson (Dwight's actor in The Office)": false
+        },
+        answer: [
+            "https://kanto.legiaodosherois.com.br/w750-h750-k1/wp-content/uploads/2021/11/legiao_A6bjpEZm4RPy.png.jpeg",
+            "The Office's list of directors is filled with unusual names, and over nine seasons it featured chapters led by people like Bryan Cranston (Breaking Bad), Jon Favreau (Iron Man), JJ Abrams (Star Trek), Jason Reitman (Ghostbusters: Beyond), among many others. In addition, the series also allowed cast actors - such as John Krasinski (Jim), Rainn Wilson (Dwight), Ed Helms (Andy), Mindy Kaling (Kelly) and others - to make their directorial debut. From the list, only Jenna Fischer who never tried to direct a chapter of the series."
+        ]
+    },
+    {
+        question: [
+            "https://kanto.legiaodosherois.com.br/w750-h450-k1/wp-content/uploads/2021/11/legiao_bpj_qiKL2l1D.png.jpeg",
+            "When Jim is transferred to Stamford's Dunder Mifflin, he discovers that the employees are addicted to a first-person shooter for PC. Which game was that?"
+        ],
+        choices: {
+            "Battlefield": false,
+            "Call of Duty": true,
+            "Halo": false,
+            "Counter-Strike": false
+        },
+        answer: [
+            "https://kanto.legiaodosherois.com.br/w750-h750-k1/wp-content/uploads/2021/11/legiao_6VWGKh7q2iCH.png.jpeg",
+            "Stamford's office was addicted to Call of Duty, one of the biggest shooting game franchises to date. According to the Office Ladies podcast (by actresses Jenna Fischer and Angela Kinsey), the reason is simple: the editors and writers of the series did the same, playing games during office hours. So they just took the opportunity to manifest the addiction within the series, adapting the real-life situation."
+        ]
+    },
+    {
+        question: [
+            "https://kanto.legiaodosherois.com.br/w750-h450-k1/wp-content/uploads/2021/11/legiao_bh0Nu9C1VE6_.png.jpeg",
+            "Steve Carell (Michael Scott) was fired from The Office"
+        ],
+        choices: {
+            "True": false,
+            "False": true
+        },
+        answer: [
+            "https://kanto.legiaodosherois.com.br/w750-h750-k1/wp-content/uploads/2021/11/legiao_AmtZVuxjUXnP.png.jpeg",
+            "Actor behind one of the series' best characters, Steve Carell hasn't been fired - but that doesn't mean he's done well. At the time, the president of the NBC network did not really understand the success of the series, and he was not keen on renewing the actor's contract. To try to gain an advantage in a negotiation, Carell announced that he was going to leave the series, but did not hear any response from the channel nor a counterproposal (even if he was willing to accept). As a result, he left the show in 2011 grudgingly, leaving the rest of the cast and crew saddened by his departure."
+        ]
+    },
+    {
+        question: [
+            "https://kanto.legiaodosherois.com.br/w750-h450-k1/wp-content/uploads/2021/11/legiao__QODCMr9Jctl.png.jpeg",
+            "Which of these actors has already auditioned for a lead role in The Office?"
+        ],
+        choices: {
+            "Kathryn Hahn (Agatha from WandaVision) to Pam": false,
+            "Seth Rogen (Superbad, It's the End, The Interview) for Dwight": false,
+            "John Cho (Seeking Cowboy Bebop) to Jim": false,
+            "All of them": true
+        },
+        answer: [
+            "https://kanto.legiaodosherois.com.br/w750-h750-k1/wp-content/uploads/2021/11/legiao_O9JI14Qayc0l.png.jpeg",
+            "Lots of talented people auditioned for The Office! The roster also includes Bob Odenkirk (the Saul Goodman of Breaking Bad and Better Call Saul) for Michael Scott, Adam Scott (Parks and Recreation) for Jim, and Patton Oswalt (Ratatouille) for Dwight. All these tests were released on the official channel of the series, and serve both to imagine an alternate reality and to prove the perfect choices of the cast of the series."
+        ]
+    },
+    {
+        question: [
+            "https://kanto.legiaodosherois.com.br/w750-h450-k1/wp-content/uploads/2021/11/legiao_10Dcdan84SBf.png.jpeg",
+            ""
+        ],
+        choices: {
+
+        },
+        answer: [
+            "",
+            ""
+        ]
+    },
+]
+
+/*
+    {
+        question: [
+            "",
+            ""
+        ],
+        choices: {
+
+        },
+        answer: [
+            "",
+            ""
+        ]
+    },
+*/
