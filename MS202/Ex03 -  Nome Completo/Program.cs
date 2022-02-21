be9d84dbe45382c456ebd9a9a0b7c9a3be0f90d5
Gabriel Beserra <gabriel.fernandesb@gmail.com>
2022-02-21 18:25:10 -0300

    Ex03 - Nome completo

---
+++ MS202/Ex03 -  Nome Completo/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.Write("First name: ");
+      var name = Console.ReadLine();
+
+      Console.Write("Last name: ");
+      var lastname = Console.ReadLine();
+
+      Console.WriteLine($"Hello, {name} {lastname}!");
+    }
+  }
+}
