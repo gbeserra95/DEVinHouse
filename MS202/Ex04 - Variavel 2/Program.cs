c63662f276ed51bf767fef7f72c844a2f8f6c43d
Gabriel Beserra <gabriel.fernandesb@gmail.com>
2022-02-21 18:26:57 -0300

    Ex04 - Variavel 2

---
+++ MS202/Ex04 - Variavel 2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Variable
+{
+  class Program
+  {
+    static void Main()
+    {
+      var nota1 = 8;
+      var nota2 = 5;
+      var nota3 = 6;
+
+      var media = (nota1 + nota2 + nota3) / 3;
+
+      Console.WriteLine(media);
+    }
+  }
+}
