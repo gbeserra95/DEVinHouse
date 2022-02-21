12065c46cbdbdc9545487feb23f237280056757c
Gabriel Beserra <gabriel.fernandesb@gmail.com>
2022-02-21 18:32:41 -0300

    Ex05 - Arredondando

---
+++ MS202/Ex05 - Arredondando/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Rounding
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.Write("Entre com um número de 2 casas decimais: ");
+
+      var n = Console.ReadLine();
+
+      Console.WriteLine(Math.Round(Convert.ToDouble(n), 0));
+    }
+  }
+}
