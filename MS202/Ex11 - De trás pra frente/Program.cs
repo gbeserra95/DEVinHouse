35663d848e736f0c9758ac055c2da6cb977954af
Gabriel Beserra <gabriel.fernandesb@gmail.com>
2022-03-09 16:13:28 -0300

    Ex10 - De tras pra frente

---
+++ MS202/Ex11 - De trás pra frente/Program.cs
@@ -0,0 +1,42 @@
+﻿void deTrasPraFrente()
+{
+  List<int> array = new List<int>();
+
+  Console.WriteLine("Input an integer: ");
+  var nString = Console.ReadLine();
+
+  int nInt;
+
+  while (!int.TryParse(nString, out nInt))
+  {
+    Console.WriteLine("This is not an integer!");
+    nString = Console.ReadLine();
+  }
+
+  for (int i = 0; i < nInt; i++)
+  {
+    Console.WriteLine("Input an integer as element of your array: ");
+    var inputString = Console.ReadLine();
+
+    int inputInteger;
+
+    while (!int.TryParse(inputString, out inputInteger))
+    {
+      Console.WriteLine("This is not an integer!");
+      inputString = Console.ReadLine();
+    }
+
+    array.Add(inputInteger);
+  }
+
+  array.Reverse();
+
+  foreach (int element in array)
+  {
+    Console.Write($"{element} ");
+  }
+
+  Console.Write("\n");
+}
+
+deTrasPraFrente();
