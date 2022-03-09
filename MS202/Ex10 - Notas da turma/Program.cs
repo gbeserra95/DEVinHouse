debe584dfa5516e1a398a9a1aa9f02e45a18db09
Gabriel Beserra <gabriel.fernandesb@gmail.com>
2022-03-09 15:25:49 -0300

    Ex10 - Notas da turma

---
+++ MS202/Ex10 - Notas da turma/Program.cs
@@ -0,0 +1,32 @@
+﻿void NotasDaTurma()
+{
+  List<int> Notas = new List<int>();
+  int maiorNota = 0;
+  int menorNota = 100;
+  int soma = 0;
+
+  for (int i = 0; i < 30; i++)
+  {
+    int nota = new Random().Next(0, 100);
+
+    if (nota > maiorNota)
+    {
+      maiorNota = nota;
+    }
+
+    if (nota < menorNota)
+    {
+      menorNota = nota;
+    }
+
+    soma += nota;
+
+    Notas.Add(nota);
+  }
+
+  Console.WriteLine($"Maior Nota: {maiorNota}");
+  Console.WriteLine($"Menor Nota: {menorNota}");
+  Console.WriteLine($"Media: {(soma / 30.00).ToString("N2")}");
+}
+
+NotasDaTurma();
